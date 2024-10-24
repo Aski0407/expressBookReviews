8bab6a543935afc41cef7fe4aea55f1c9f39541e
Aski0407 <nataly.golubitsky@gmail.com>
2024-10-24 12:42:02 -0400

    need to check login

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,28 +5,9 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username) => { //returns boolean
-    //write code to check is the username is valid
-    const filtered_users = users.filter((user)=> user.username === user);
-    if (filtered_users){
-        return true;
-    }
-    return false;
-}
-
 const authenticatedUser = (username, password) => { //returns boolean
     //write code to check if username and password match the one we have in records.
-    // Filter the users array for any user with the same username and password
-    if(isValid(username)){
-        const filtered_users = users.filter((user)=> (user.username===username)&&(user.password===password));
-        if(filtered_users){
-            return true;
-        }
-        return false;
-       
-    }
-    return false;
-    
+    return users.find((user) => (user.username===username)&&(user.password===password));    
 }
 
 //only registered users can login
@@ -48,13 +29,12 @@ regd_users.post("/login", (req, res) => {
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
     const isbn = req.params.isbn;
-    let filtered_book = books[isbn]
-    if (filtered_book) {
+    let bookInfo = books[isbn]
+    if (bookInfo) {
         let review = req.query.review;
         let reviewer = req.session.authorization['username'];
         if(review) {
-            filtered_book['reviews'][reviewer] = review;
-            books[isbn] = filtered_book;
+            bookInfo.reviews[reviewer] = review;
         }
         res.send(`The review for the book with ISBN  ${isbn} has been added/updated.`);
     }
@@ -64,5 +44,4 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
 });
 
 module.exports.authenticated = regd_users;
-module.exports.isValid = isValid;
 module.exports.users = users;
